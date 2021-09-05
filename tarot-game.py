83d43a8e07f14bf1fbe4651ec332b2aaa74544f1
FluroAsh <workashleythompson@gmail.com>
2021-09-05 17:39:46 +1000

    Added classes

--- tarot-game.py
+++ tarot-game.py
@@ -1,4 +1,16 @@
-import json
+# import json
 
-with open('C:\Users\Orang\Documents\GitHub\tarot-data.jason') as f:
-    data = json.load(f)
+# with open('C:\Users\Orang\Documents\GitHub\tarot-data.jason') as f:
+#     data = json.load(f)
+
+class Card:
+    def __init__(self):
+        pass
+
+class Deck:
+    def __init__(self):
+        pass
+
+class Player:
+    def __init__(self):
+        pass
