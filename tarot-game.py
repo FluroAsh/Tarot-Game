87274d4f1697534789c42d63183265031a95b01e
FluroAsh <workashleythompson@gmail.com>
2021-09-07 12:09:54 +1000

    Moved functions into class, and did some optimisations with string concatenation. Changed some of the up/rev logic.

--- tarot-game.py
+++ tarot-game.py
@@ -30,20 +30,14 @@ print("       )        (")
 print("      (          )")
 print("       `-......-`lc\n")
 
-random_cards = []
-
-# Get's 3 RANDOM cards & their ATTRIBUTES
-for i in range(3): 
-    random_i = random.randint(0,77)
-    # Add code to PREVENT duplicates!!
-    random_cards.append(data['cards'][random_i])
 
 class Player:
     def __init__(self, name):
         self.name = name 
 
-
 class Card:
+    random_cards = []
+    meaning_u_r = 0
     def __init__(self, name, name_short, value, value_int, identifier, meaning_up, meaning_rev, desc):
         self.name = name
         self.name_short = name_short
@@ -53,40 +47,74 @@ class Card:
         self.meaning_up = meaning_up
         self.meaning_rev = meaning_rev
         self.desc = desc
+    
+    def get_cards():
+        for i in range(3): 
+            random_i = random.randint(0,77)
+            # Add code to PREVENT duplicates!!
+            Card.random_cards.append(data['cards'][random_i])
+
+    def up_rev():
+        Card.meaning_u_r = random.randint(1,16)
+
 
 print('\t' + intro + '\n\n')
-player_name = Player(input("What is your name?: "))
+player_name = Player(str(input("What is your name?: ")))
 time.sleep(1)
-print("Welcome " + player_name.name + " may the cards bless you with good fortune...\n")
+print(">>> Welcome " + player_name.name.title() + " may the cards bless you with good fortune...\n")
 time.sleep(3)
+
+# Get's 3 RANDOM cards & their ATTRIBUTES
+Card.get_cards()
 
 # Initialize our randomly generated cards (between 1-78)
-card1 = Card(random_cards[0]["name"], random_cards[0]["name_short"], random_cards[0]["value"], random_cards[0]["value_int"], random_cards[0]["type"], random_cards[0]["meaning_up"], random_cards[0]["meaning_rev"], random_cards[0]["desc"])
-card2 = Card(random_cards[1]["name"], random_cards[1]["name_short"], random_cards[1]["value"], random_cards[1]["value_int"], random_cards[1]["type"], random_cards[1]["meaning_up"], random_cards[1]["meaning_rev"], random_cards[1]["desc"])
-card3 = Card(random_cards[2]["name"], random_cards[2]["name_short"], random_cards[2]["value"], random_cards[2]["value_int"], random_cards[2]["type"], random_cards[2]["meaning_up"], random_cards[2]["meaning_rev"], random_cards[2]["desc"])
+card1 = Card(Card.random_cards[0]["name"], Card.random_cards[0]["name_short"], Card.random_cards[0]["value"], Card.random_cards[0]["value_int"], Card.random_cards[0]["type"], Card.random_cards[0]["meaning_up"], Card.random_cards[0]["meaning_rev"], Card.random_cards[0]["desc"])
+card2 = Card(Card.random_cards[1]["name"], Card.random_cards[1]["name_short"], Card.random_cards[1]["value"], Card.random_cards[1]["value_int"], Card.random_cards[1]["type"], Card.random_cards[1]["meaning_up"], Card.random_cards[1]["meaning_rev"], Card.random_cards[1]["desc"])
+card3 = Card(Card.random_cards[2]["name"], Card.random_cards[2]["name_short"], Card.random_cards[2]["value"], Card.random_cards[2]["value_int"], Card.random_cards[2]["type"], Card.random_cards[2]["meaning_up"], Card.random_cards[2]["meaning_rev"], Card.random_cards[2]["desc"])
 
 # -- Fortune Teller Text
-print("Your first card...\n")
-time.sleep(3)
-print(card1.name + '\n' + card1.desc + '\n')
+
+## CARD 1
+print(">>> The PAST...\n")
 time.sleep(3)
 
-print("Fate has a sense of humour, it seems...\n")
+Card.up_rev() 
+if Card.meaning_u_r <= 10: # Logic to determine up or reverse orientation -- 66% chance to get up
+    print('[' + card1.name.upper() + ']' + '\n' + card1.desc + '\n\n' + card1.meaning_up + '\n')
+else:
+    print('[' + card1.name.upper()  + ']' + '\n' + card1.desc + '\n\n' + '**REVERSED: ' + card1.meaning_rev + '\n')
+time.sleep(15)
+
+print(">>> Fate has a sense of humour, it seems...\n")
 time.sleep(8)
 
-print("Your second card...\n")
-time.sleep(3)
-print(card2.name + '\n' + card2.desc + '\n')
+## CARD 2 
+print(">>> The PRESENT...\n")
 time.sleep(3)
 
-print("Hmmm... Interesting...\n")
-time.sleep(8)
+Card.up_rev() # Refresh our meaning_u_r variable
+if Card.meaning_u_r <= 10:
+    print('[' + card2.name.upper() + ']' + '\n' + card2.desc + '\n\n' + card2.meaning_up + '\n')
+else:
+    print('[' + card2.name.upper() + ']' + '\n' + card2.desc + '\n\n' + '**REVERSED: ' + card2.meaning_rev + '\n')
+time.sleep(15)
 
-print("Your third, and FINAL card...\n ")
+print(">>> Hmmm... Interesting...\n")
+time.sleep(10)
+
+## CARD 3
+print(">>> Your third, and FINAL card... The FUTURE!\n")
 time.sleep(3)
-print(card3.name + '\n' + card3.desc + '\n')
 
-time.sleep(8)
+Card.up_rev()
+if Card.meaning_u_r <= 10:
+    print('[' + card3.name.upper()  + ']' + '\n' + card3.desc + '\n\n' + card3.meaning_up + '\n')
+else:
+    print('[' + card3.name.upper()  + ']' + '\n' + card3.desc + '\n\n' + '**REVERSED: ' + card3.meaning_rev + '\n')
+time.sleep(15)
+
+print('----------------------------------------------------------------------------------------------------------------------')
 print("***Though your fate is in the cards, your choices define your path... Let them guide you to your final destination.***")
 
+# Time out after 30 seconds
 time.sleep(30)
